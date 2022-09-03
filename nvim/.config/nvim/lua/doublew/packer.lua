00c9da1a2a43de5db0a094d216cae806434bc157
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-09-03 21:57:47 +0800

    feat(nvim): nvim-treesitter-context for sticky matching text

--- nvim/.config/nvim/lua/doublew/packer.lua
+++ nvim/.config/nvim/lua/doublew/packer.lua
@@ -63,6 +63,7 @@ return packer.startup(function(use)
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
 	})
+	use("nvim-treesitter/nvim-treesitter-context")
 	use("p00f/nvim-ts-rainbow")
 	use("nvim-treesitter/playground")
 	use("jose-elias-alvarez/null-ls.nvim")
