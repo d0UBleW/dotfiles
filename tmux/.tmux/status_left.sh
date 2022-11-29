c641754f9c040f111447f5c84827bc4fdd79da21
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2022-11-29 21:56:46 +0800

    feat(tmux): status_left

---
+++ tmux/.tmux/status_left.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function ipv4() {
+    printf "🌐 #[fg=yellow]%s#[default]" "$(ip address show eth0 | grep -oP '(?<=inet ).*(?= brd)')"
+}
+
+function sep() {
+    printf " | "
+}
+
+function session_name() {
+    printf "#[fg=green]#S #[default]> "
+}
+
+function main() {
+    ipv4
+    sep
+    session_name
+}
+
+# Calling the main function which will call the other functions.
+main
