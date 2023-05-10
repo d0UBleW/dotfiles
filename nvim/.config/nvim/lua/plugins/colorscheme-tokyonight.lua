6077c83be4d5d758771835af5ed753e5674dbd76
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-05-10 10:39:54 +0800

    chore: adjustment for void on hyper-v with xrdp

--- nvim/.config/nvim/lua/plugins/colorscheme-tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme-tokyonight.lua
@@ -24,7 +24,7 @@ return {
 		end,
 		config = function(_, opts)
 			require("tokyonight").setup(opts)
-			-- vim.cmd([[ colorscheme tokyonight ]])
+			vim.cmd([[ colorscheme tokyonight ]])
 		end,
 	},
 }
