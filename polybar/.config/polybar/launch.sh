0376ccf7e88fb1099ca3eb414d27b56ba1b67ff2
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-05-11 20:05:04 +0800

    chore: stuffs

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,9 +2,9 @@
 
 pkill polybar
 
-# polybar top 2>&1 | tee -a /tmp/polybar.log & disown
-polybar left 2>&1 | tee -a /tmp/polybar-left.log & disown
-polybar mid 2>&1 | tee -a /tmp/polybar-mid.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar-right.log & disown
+polybar top 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar left 2>&1 | tee -a /tmp/polybar-left.log & disown
+# polybar mid 2>&1 | tee -a /tmp/polybar-mid.log & disown
+# polybar right 2>&1 | tee -a /tmp/polybar-right.log & disown
 
 echo "Polybar launched..."
