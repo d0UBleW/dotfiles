cc2d644bfaecd905c9baba43e9b4f4c4e631785d
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-06-03 09:40:36 +0800

    chore(nvim): spider dot-repeatable

--- nvim/.config/nvim/lua/plugins/spider.lua
+++ nvim/.config/nvim/lua/plugins/spider.lua
@@ -5,15 +5,18 @@ return {
 		keys = function()
 			local lazy_keymap = require("doublew.keymap").lazy_keymap
 			return {
-				lazy_keymap("<A-w>", function()
-					require("spider").motion("w")
-				end),
-				lazy_keymap("<A-e>", function()
-					require("spider").motion("e")
-				end),
-				lazy_keymap("<A-b>", function()
-					require("spider").motion("b")
-				end),
+				lazy_keymap("<A-w>", "<cmd>lua require('spider').motion('w')<CR>", {
+					desc = "Spider-w",
+					mode = { "n", "o", "x" },
+				}),
+				lazy_keymap("<A-e>", "<cmd>lua require('spider').motion('e')<CR>", {
+					desc = "Spider-e",
+					mode = { "n", "o", "x" },
+				}),
+				lazy_keymap("<A-b>", "<cmd>lua require('spider').motion('b')<CR>", {
+					desc = "Spider-b",
+					mode = { "n", "o", "x" },
+				}),
 			}
 		end,
 	},
