a12c7862693c964791122ad0f9c73e64e5a6be06
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-06-09 15:05:20 +0800

    feat: rounded

--- polybar/.config/polybar/scripts/get_spotify_status.sh
+++ polybar/.config/polybar/scripts/get_spotify_status.sh
@@ -39,12 +39,12 @@ else
     if [ "$STATUS" = "Stopped" ]; then
         echo "No music is playing"
     elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
+        # update_hooks "$PARENT_BAR_PID" 2
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     elif [ "$STATUS" = "No player is running"  ]; then
         echo "$STATUS"
     else
-        update_hooks "$PARENT_BAR_PID" 1
+        # update_hooks "$PARENT_BAR_PID" 1
         playerctl --player=$PLAYER metadata --format "$FORMAT"
     fi
 fi
--- polybar/.config/polybar/scripts/scroll_spotify_status.sh
+++ polybar/.config/polybar/scripts/scroll_spotify_status.sh
@@ -4,7 +4,7 @@
 zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding "  " \
-        --match-command "/home/d0ublew/.config/polybar/scripts/get_spotify_status.sh --status" \
+        --match-command "playerctl status 2>/dev/null" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true "/home/d0ublew/.config/polybar/scripts/get_spotify_status.sh" &
