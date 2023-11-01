87eebb9f185e73276bfab210b2b60adb405e5157
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-11-01 11:36:30 +0800

    feat(nvim): mini.bufremove to close buffer

--- nvim/.config/nvim/after/ftplugin/netrw.lua
+++ nvim/.config/nvim/after/ftplugin/netrw.lua
@@ -1,4 +1,11 @@
 local keymap = require("util.keymaps")
 local buf_nnoremap = keymap.buf_nnoremap
 
-buf_nnoremap(0, "<esc>", "<cmd>bd<cr>", { desc = "Close NetRW" })
+local bufremove_ok, bufremove = pcall(require, "mini.bufremove")
+if bufremove_ok then
+	buf_nnoremap(0, "<esc>", function()
+		bufremove.delete(0, false)
+	end, { desc = "Close NetRW (bufremove)" })
+else
+	buf_nnoremap(0, "<esc>", "<cmd>bd<cr>", { desc = "Close NetRW" })
+end
