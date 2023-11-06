ee072bd032b68b7f5a7f33f326957bf3ca09878b
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-11-06 20:15:42 +0800

    chore(nvim): oil keymaps

--- nvim/.config/nvim/lua/plugins/util.lua
+++ nvim/.config/nvim/lua/plugins/util.lua
@@ -3,28 +3,39 @@ return {
 	{
 		"stevearc/oil.nvim",
 		lazy = false,
-		-- opts = {
-		-- },
-		config = function()
-			require("oil").setup({
-				columns = {
-					-- "icon",
-				},
-				cleanup_delay_ms = 0,
-				keymaps = {
-					["<A-p>"] = "actions.preview",
-					["<A-u>"] = "actions.preview_scroll_up",
-					["<A-d>"] = "actions.preview_scroll_down",
-					["<A-y>"] = "actions.copy_entry_path",
-				},
-			})
-		end,
+		opts = {
+			columns = {
+				-- "icon",
+			},
+			cleanup_delay_ms = 0,
+			keymaps = {
+				["<A-p>"] = "actions.preview",
+				["<A-u>"] = "actions.preview_scroll_up",
+				["<A-d>"] = "actions.preview_scroll_down",
+				["<A-y>"] = "actions.copy_entry_path",
+			},
+		},
 		keys = function()
 			local lazy_keymap = require("util.keymaps").lazy_keymap
+			local oil = require("oil")
 			return {
 				lazy_keymap("-", "<cmd>Oil<cr>", {
 					mode = "n",
 					desc = "Open Oil File Explorer",
+				}),
+				lazy_keymap('<A-">', function()
+					oil.set_columns({
+						"permissions",
+					})
+				end, {
+					mode = "n",
+					desc = "Show permissions (oil)",
+				}),
+				lazy_keymap("<A-'>", function()
+					oil.set_columns({})
+				end, {
+					mode = "n",
+					desc = "Hide permissions (oil)",
 				}),
 			}
 		end,
