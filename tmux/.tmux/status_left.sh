ba843d63847d52b691839ffe51ff41760c5bd6a7
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2023-12-07 10:32:31 +0800

    feat: tmux stuff

--- tmux/.tmux/status_left.sh
+++ tmux/.tmux/status_left.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 function ipv4() {
-    printf "#[fg=yellow]%s#[default]" "$(ip address show eth0 | grep -oP '(?<=inet ).*(?= brd)')"
+	printf "#[fg=yellow]%s#[default]" "$(ip address show eth0 | grep -oP '(?<=inet ).*(?= brd)')"
 }
 
 function sep() {
-    printf " | "
+	printf " | "
 }
 
 function session_name() {
-    # printf "#[fg=green]#S #[default]> "
-    # printf "#[fg=brightblack]#[fg=brightwhite,bg=brightblack,bold] #S #[default]#[fg=brightblack,nobold,noitalics,nounderscore] "
-    printf "#[fg=brightwhite,bg=brightblack,bold] #S #[default]"
+	# printf "#[fg=green]#S #[default]> "
+	# printf "#[fg=brightblack]#[fg=brightwhite,bg=brightblack,bold] #S #[default]#[fg=brightblack,nobold,noitalics,nounderscore] "
+	printf "#[fg=#ffffff,bg=brightblack,bold] #S #[default]"
 }
 
 function main() {
-    session_name
-    printf " "
+	session_name
+	printf " "
 }
 
 # Calling the main function which will call the other functions.
