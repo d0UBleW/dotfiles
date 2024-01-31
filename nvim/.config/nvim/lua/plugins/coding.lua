0e6f730fff1c21c1802b6e759e624a702e0cad22
d0UBleW <66501624+d0UBleW@users.noreply.github.com>
2024-01-31 20:16:14 +0800

    chore: change keymap for snippet jump

--- nvim/.config/nvim/lua/plugins/coding.lua
+++ nvim/.config/nvim/lua/plugins/coding.lua
@@ -18,14 +18,14 @@ return {
 		-- stylua: ignore
 		keys = {
 		  {
-			"<tab>",
+			"<A-n>",
 			function()
 			  return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
 			end,
 			expr = true, silent = true, mode = "i",
 		  },
-		  { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-		  { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+		  { "<A-n>", function() require("luasnip").jump(1) end, mode = "s" },
+		  { "<A-p>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
 		},
 	},
 
